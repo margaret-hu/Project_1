bf930982acbf64a3e72596d41baaff5a109bbc0d
margaret-hu <margaret_hu@outlook.com>
2021-02-08 20:59:31 -0800

    Initial commit

---
+++ src/edu/mhu/address/AddressBookApplication.java
@@ -0,0 +1,15 @@
+package edu.mhu.address;
+
+public class AddressBookApplication {
+
+    public static void main(String[] args) {
+        Menu.prompt_FirstName();
+        Menu.prompt_LastName();
+        Menu.prompt_Street();
+        Menu.prompt_City();
+        Menu.prompt_State();
+        Menu.prompt_Zip();
+        Menu.prompt_Telephone();
+        Menu.prompt_Email();
+    }
+}
---
+++ src/edu/mhu/address/Menu.java
@@ -0,0 +1,67 @@
+package edu.mhu.address;
+
+public class Menu {
+    /**
+     * prompt_FirstName  generates a standard output prompt for the First Name to be entered
+     */
+    public static void prompt_FirstName()
+    {
+        System.out.println("First Name:");
+    }
+
+    /**
+     * prompt_LastName  generates a standard output prompt for the Last Name to be entered
+     */
+    public static void prompt_LastName()
+    {
+        System.out.println("Last Name:");
+    }
+
+    /**
+     * prompt_Street  generates a standard output prompt for the Street to be entered
+     */
+    public static void prompt_Street()
+    {
+        System.out.println("Street:");
+    }
+
+    /**
+     * prompt_City  generates a standard output prompt for the City to be entered
+     */
+    public static void prompt_City()
+    {
+        System.out.println("City:");
+    }
+
+    /**
+     * prompt_State  generates a standard output prompt for the State to be entered
+     */
+    public static void prompt_State()
+    {
+        System.out.println("State:");
+    }
+
+    /**
+     * prompt_Zip  generates a standard output prompt for the Zip to be entered
+     */
+    public static void prompt_Zip()
+    {
+        System.out.println("Zip:");
+    }
+
+    /**
+     * prompt_Telephone  generates a standard output prompt for the Telephone to be entered
+     */
+    public static void prompt_Telephone()
+    {
+        System.out.println("Telephone:");
+    }
+
+    /**
+     * prompt_Email  generates a standard output prompt for the Email to be entered
+     */
+    public static void prompt_Email()
+    {
+        System.out.println("Email:");
+    }
+}
