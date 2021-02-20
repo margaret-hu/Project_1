74ad6d3acac221ad4039ec27853659cc742488d3
margaret-hu <margaret_hu@outlook.com>
2021-02-19 22:51:25 -0800

    Tested add, remove, and list function

--- src/edu/mhu/address/TestAddressBook.java
+++ src/edu/mhu/address/TestAddressBook.java
@@ -8,10 +8,39 @@ public class TestAddressBook {
         AddressEntry e3 = new AddressEntry("Diana", "Huang", "4770 Mallard", "Fremont", "CA", 94555, "(510)709-5158", "diana.w.hu@gmail.com ");
         AddressEntry e4 = new AddressEntry("Iliana", "Bicakci", "1 Harpst St", "Arcata", "CA", 95521, "(510)912-6169", "iliana_b@outlook.com");
         AddressBook ab = new AddressBook();
-        AddressEntry[] tests = {e2, e1, e3, e4};
+        AddressEntry[][] tests = {{e1, e2, e3, e4}, {e2, e3, e1, e4}, {e4, e3, e2, e1}};
+        for (AddressEntry[] ts : tests) {
+            for (int i = 0; i < ts.length; i++) {
+                if (i == 0) {
+                    ab.add(ts[i]);
+                } else if (i == 1) {
+                    ab.add(ts[i]);
+                    ab.list();
+                } else if (i == 2) {
+                    ab.add(ts[i]);
+                    ab.find("Hu");
+                    ab.remove(ts[1]);
+                } else if (i == 3) {
+                    ab.list();
+                    ab.remove(ts[2]);
+                    ab.list();
+                }
+            }
+            for (AddressEntry e : ts) {
+                ab.remove(e);
+            }
+        }
+        /*AddressEntry[] tests = {e2, e1, e3, e4};
         for (AddressEntry e : tests) {
             ab.add(e);
         }
         AddressEntry[] arr = ab.remove("Hu");
+        ab.find("Hu");
+        for (AddressEntry e : tests) {
+            ab.list();
+            ab.remove(e);
+            System.out.println("Removing " + e.getFirstName());
+        }
+        ab.remove(e4);*/
     }
 }
